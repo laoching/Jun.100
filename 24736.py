12ec6f6928ceea1987a8f1281efdfdbd3077e4a6
laoching <wjdtkdals159@gmail.com>
2022-06-01 16:45:09 +0900

    24736

---
+++ 24736.py
@@ -0,0 +1,4 @@
+t1, f1, s1, a1, one1, two1 = map(int, input().split())
+t2, f2, s2, a2, one2, two2 = map(int, input().split())
+print((t1 * 6) + (f1 * 3) + (a1 * 2) + (one1 * 1) + (two1 * 2))
+print((t2 * 6) + (f2 * 3) + (a2 * 2) + (one2 * 1) + (two2 * 2))
