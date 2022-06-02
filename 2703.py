c17a745162c09eee819750231c4d2e1156d43a37
laoching <wjdtkdals159@gmail.com>
2022-06-03 05:46:27 +0900

    2703

--- 2703.py
+++ 2703.py
@@ -8,26 +8,34 @@ for _ in alpha:
 for i in range(t):
     
     cnt_rule = 1
-    cnt_plain = 1
+    cnt_cypertext = 1
     match_rule = {}
-    match_plain = []
+    match_cypertext = []
     result = []
 
-    plain = input()
+    cypertext = input()
     rule = input()
 
-    for j in plain:
-        plain_index = alpha_dick.get(j)
-        match_plain.append(plain_index)
+    for j in cypertext:
+        cypertext_index = alpha_dick.get(j)
+        match_cypertext.append(cypertext_index)
 
     for i in rule:
         match_rule[cnt_rule] = i
         cnt_rule += 1
     
-    for k in match_plain:
+    for k in match_cypertext:
         crypto = match_rule.get(k)
         if crypto is None:
             crypto = ' '
         result.append(crypto)
 
     print(''.join(result))
+
+"""
+for i in cypertext:
+    if i == ' ':
+        print(' ', end = "")
+    else:
+        print(key[ord(i) - ord("A")], end="")
+"""
