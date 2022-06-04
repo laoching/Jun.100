603e3247fc89381fcdd8016c9a724220fb6eabbb
laoching <wjdtkdals159@gmail.com>
2022-06-04 16:07:49 +0900

    10250

---
+++ 10250.py
@@ -0,0 +1,8 @@
+t = int(input())
+for i in range(t):
+    arr = []
+    h, w, n = map(int, input().split())
+    for j in range(1, w + 1):
+        for k in range(1, h + 1):
+            arr.append((100 * k) + j)
+    print(arr[n - 1])
