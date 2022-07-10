4b9a55a0912d43429e3d69703f827a3bfc26fbc3
laoching <wjdtkdals159@gmail.com>
2022-07-10 17:35:14 +0900

    192

--- 1920.py
+++ 1920.py
@@ -1,7 +1,7 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
-a = list(map(int, input().split()))
+a = set(map(int, input().split())) # set을 써서 중복을 다 죽임 -> list보다 탐색 시간이 더 빠름
 n1 = int(input())
 b = list(map(int, input().split()))
 for i in b:
